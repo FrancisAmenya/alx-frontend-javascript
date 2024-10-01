89c6b8ee79e7ddb356aa9deb3eba88ae12978f7b
FrancisAmenya <amenyafrancis@gmail.com>
2024-10-01 01:25:05 +0000

    edge fixes across

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,10 +4,9 @@ export default function taskBlock(trueOrFalse) {
 
   if (trueOrFalse) {
     /* eslint-disable no-unused-vars */
-    const task = true;
-    const task2 = false;
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
 }
-
